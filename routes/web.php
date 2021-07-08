02ba1568ff30b99826c121a04c99feb75597d4a3
Филип Арсовски <arsovskidev@gmail.com>
2021-07-08 14:25:28 +0200

    Removed breeze/tailwind, installed laravel/ui bootstrap.

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Auth::routes();
 
-require __DIR__.'/auth.php';
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
